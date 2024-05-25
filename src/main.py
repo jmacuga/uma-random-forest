9a6f6be84a5728d0076ac624089d51981df1fef9
Julia Macuga <julia.macuga@gmail.com>
2024-05-25 21:14:22 +0200

    confusion matricec

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from datasets.mushrooms import MushroomDataset
 from tree import DecisionTreeClassifier
 from random_forest import RandomForestClassifier, TournamentRandomForestClassifier
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, accuracy_score
+
 
 if __name__ == "__main__":
     path = "./data/agaricus-lepiota.data"
@@ -10,14 +10,27 @@ if __name__ == "__main__":
     dataset.clean()
     X_train, X_val, y_train, y_val = dataset.split(test_size=0.2, random_state=42)
 
-    rf = RandomForestClassifier(10, 3)
-    rf.fit(X_train, y_train)
-    correct = 0
-    for sample, target in zip(X_val, y_val):
-        prediction = rf.predict(sample)
-        if prediction == target:
-            correct += 1
-        print("Prediction: " + str(prediction) + " Target: " + str(target))
-    print("\nCorrect: " + str(correct))
+    print(len(X_train))
+    print(X_train.shape)
+    print(len(y_train))
+    print(y_train.shape)
+    # print(y_val)
+    # dc.get_best_split(X_train, y_train)
+
+    n_features = round(math.sqrt(X_train.shape[1]))
+    # dc = RandomizedDecisionTreeClassifier(3, max_features=n_features)
+    dc = DecisionTreeClassifier(7)
+
+    start = time.process_time()
+    dc.fit(X_train, y_train)
+    print(f"Time: {time.process_time() - start}s")
+
+    y_preds = dc.predict(X_val)
+
+    accuracy = accuracy_score(y_val, y_preds)
+    confusion_matrix = confusion_matrix(y_val, y_preds)
+
     print("Num of test samples: " + str(len(X_val)))
-    print("Accuracy: " + str(correct / len(X_val)))
+    print(f"Accuracy: {accuracy}")
+    print("Confusion matrix:")
+    print(confusion_matrix)
