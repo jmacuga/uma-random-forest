f83e6de13208c73118225eb94054a719f0e9692f
Julia Macuga <julia.macuga@gmail.com>
2024-05-30 10:55:47 +0200

    covertype dataset inspection

---
+++ src/datasets/covertype.py
@@ -0,0 +1,37 @@
+import numpy as np
+from pandas.core.api import DataFrame as DataFrame
+from utils.types import Dataset
+import pandas as pd
+from ucimlrepo import fetch_ucirepo
+
+
+class CovertypeDataset(Dataset):
+    def __init__(self, path: str = None):
+        super().__init__()
+        if path is None:
+            covertypes = fetch_ucirepo(id=31)
+
+            self.data = np.array(covertypes.data.features)
+            self.labels = np.array(covertypes.data.targets)
+        else:
+            covertypes = pd.read_csv(path, header=None)
+            self.data = covertypes.iloc[:, :-1]
+            self.labels = covertypes.iloc[:, -1]
+
+        self.features_map = {}
+        self.classes_map = {}
+        self.labels = (
+            "Spruce/Fir",
+            "Lodgepole Pine",
+            "Ponderosa Pine",
+            "Cottonwood/Willow",
+            "Aspen",
+            "Douglas-fir",
+            "Krummholz",
+        )
+
+    def __repr__(self):
+        return f"CovertypeDataset with {len(self)} samples"
+
+    def clean(self) -> None:
+        pass
